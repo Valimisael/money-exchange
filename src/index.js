a40d05537c683c71d99c86b6d05d51f1b0181953
Valeryia Himzheuskaya <valimisael@yahoo.com>
2019-02-21 16:54:10 +0300

    Money Exchange Solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    var exchange = {},
+        currencyLeft = currency,
+        objectOfValues = {
+            "H" : 50,
+            "Q" : 25,
+            "D" : 10,
+            "N" : 5,
+            "P" : 1
+        }
+        for (var key in objectOfValues) {
+            getExchange(objectOfValues[key], key);
+        }
+        function getExchange(value, key) {
+            if(currencyLeft/value >= 1) {
+                var count = Math.floor(currencyLeft/value);
+                exchange[key] = count;
+                currencyLeft -= count * value;
+            }
+        }
+    return exchange;
 }
